255316ee07d82399adc5aaf810ba77b3586da27c
SepiGabi <sepigabi@gmail.com>
2018-04-05 10:09:27 +0200

    Update Recursion.sql

--- SQL/Recursion.sql
+++ SQL/Recursion.sql
@@ -11,3 +11,39 @@ end try
 begin catch
        select ERROR_NUMBER(), ERROR_MESSAGE()
 end catch
+
+------------------------------------------------------------------
+
+create procedure GetDescendantLocationIds
+(
+	@LocationId int
+)
+as
+begin
+	-- v1.0
+
+ ;with CTE
+    (
+        Id
+    )
+    as 
+    (
+        select
+            ABS(l2.Id)
+        from Location l1
+		join Location l2
+			on l2.ParentLocationId = l1.Id
+		where l1.Id = @LocationId
+
+        union all
+        select
+           ABS(l.Id)
+        from Location l
+        join CTE c
+			on c.Id = l.ParentLocationId
+    )
+    
+    select distinct * 
+    from CTE
+
+end

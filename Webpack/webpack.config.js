6179af557d1878b21f996276d2774bab8afc0e7d
SepiGabi <sepigabi@gmail.com>
2018-04-12 10:24:42 +0200

    Update webpack.config.js

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -1,6 +1,13 @@
-var environment = (process.env.NODE_ENV || "production").trim();
-if (environment === "development") {
+/// <binding AfterBuild='Run - Development' />
+
+var fs = require('fs');
+var configuration = fs.readFileSync("BuildConfiguration.txt", 'ascii').split(/[\n\r]/)[0];
+
+if (configuration === "Debug") {
     module.exports = require('./webpack.development.config.js');
 } else {
     module.exports = require('./webpack.production.config');
 }
+
+//TODO: a project pre-build eventjébe bele kell tenni: 
+//echo $(ConfigurationName)>"$(ProjectDir)BuildConfiguration.txt"

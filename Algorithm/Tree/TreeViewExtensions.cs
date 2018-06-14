df843f71c553414e33a71b3ba3dc04b070911dc4
SepiGabi <sepigabi@gmail.com>
2018-06-14 13:02:13 +0200

    Create TreeViewExtensions.cs

---
+++ Algorithm/Tree/TreeViewExtensions.cs
@@ -0,0 +1,50 @@
+namespace TreeExample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class TreeViewExtensions
+    {
+        public static ObjectTreeRoot ToObjectTreeRoot(this IEnumerable<ObjectTreeItem> objectTreeItems)
+        {
+            ObjectTreeRoot objectTreeRoot = new ObjectTreeRoot();
+
+            HashSet<ObjectTreeItem> itemsSet = new HashSet<ObjectTreeItem>(objectTreeItems);
+            foreach(ObjectTreeItem objectTreeItem in itemsSet)
+            {
+                if (String.IsNullOrEmpty(objectTreeItem.ParentId))
+                {
+                    objectTreeRoot.Nodes.Add(new ObjectTreeNode() { Id = objectTreeItem.Id, DisplayName = objectTreeItem.DisplayName, Link = objectTreeItem.Link });
+                    itemsSet.Remove(objectTreeItem);
+                }
+            }
+
+            while(itemsSet.Count > 0)
+            {
+                ObjectTreeItem item = itemsSet.First();
+                if (IsExistNode(objectTreeRoot, item.ParentId, out ObjectTreeNode node))
+                {
+                    node.Children.Add(new ObjectTreeNode() { Id = item.Id, DisplayName = item.DisplayName, Link = item.Link });
+                    itemsSet.Remove(item);
+                }
+            }
+
+            return objectTreeRoot;
+        }
+
+        private static Boolean IsExistNode(ObjectTreeRoot objectTreeRoot, String NodeId, out ObjectTreeNode foundedNode)
+        {
+            foundedNode = null;
+            foreach(ObjectTreeNode node in objectTreeRoot.Nodes)
+            {
+                foundedNode = node.Find(n => n.Id == NodeId);
+                if(foundedNode != null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}

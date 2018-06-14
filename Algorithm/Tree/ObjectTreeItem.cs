55c4582b4e0fe494fba4fcae5fb94f23172c35ea
SepiGabi <sepigabi@gmail.com>
2018-06-14 13:00:31 +0200

    Create ObjectTreeItem.cs

---
+++ Algorithm/Tree/ObjectTreeItem.cs
@@ -0,0 +1,15 @@
+namespace TreeExample
+{
+    using System;
+
+    public class ObjectTreeItem
+    {
+        public String DisplayName { get; set; }
+
+        public String Link { get; set; }
+
+        public String Id { get; set; }
+
+        public String ParentId { get; set; }
+    }
+}

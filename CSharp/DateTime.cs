bd4a357aee32c662edfab1b3a95f7dc4758f29cb
SepiGabi <sepigabi@gmail.com>
2018-06-26 13:37:02 +0200

    Create DateTime.cs

---
+++ CSharp/DateTime.cs
@@ -0,0 +1,12 @@
+DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Unspecified)
+//2018-06-26T12:34:19.6712406
+
+DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Local)
+//2018-06-26T12:34:19.6712406+02:00
+
+DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc)
+//2018-06-26T12:34:19.6712406Z
+
+DateTime.Now
+//2018-06-26T12:34:19.6712406+02:00
+

a7b8a66613133f1da93ce28bb1414b841129975a
SepiGabi <sepigabi@gmail.com>
2019-04-23 16:06:03 +0200

    Update DbContextExtensions.cs

--- EntityFW/DbContextExtensions.cs
+++ EntityFW/DbContextExtensions.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Binarit.Framework.BusinessObject.Domain;
 using Microsoft.EntityFrameworkCore;
@@ -11,12 +12,14 @@ namespace MobileClient.Portable.Extensions
         public static void AddOrUpdate<T>( this DbSet<T> dbSet, IEnumerable<T> records )
         where T : BusinessObjectBaseWithId
         {
+            var dbContext = dbSet.GetContext();
             foreach (var data in records)
             {
                 var exists = dbSet.AsNoTracking().Any( x => x.Id == data.Id );
                 if (exists)
                 {
                     dbSet.Update( data );
+                    dbContext.Entry( data ).State = EntityState.Modified;
                     continue;
                 }
                 dbSet.Add( data );
@@ -26,16 +29,27 @@ namespace MobileClient.Portable.Extensions
         public static async Task AddOrUpdateAsync<T>( this DbSet<T> dbSet, IEnumerable<T> records )
         where T : BusinessObjectBaseWithId
         {
+            var dbContext = dbSet.GetContext();
             foreach (var data in records)
             {
                 var exists = dbSet.AsNoTracking().Any( x => x.Id == data.Id );
                 if (exists)
                 {
                     dbSet.Update ( data );
+                    dbContext.Entry( data ).State = EntityState.Modified;
                     continue;
                 }
                 await dbSet.AddAsync( data );
             }
+        }
+
+        public static DbContext GetContext<TEntity>( this DbSet<TEntity> dbSet )
+        where TEntity : class
+        {
+            return (DbContext) dbSet
+                .GetType().GetTypeInfo()
+                .GetField( "_context", BindingFlags.NonPublic | BindingFlags.Instance )
+                .GetValue( dbSet );
         }
     }
 }

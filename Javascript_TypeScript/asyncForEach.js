b2d036158f219b4d4186d446b0ff4e0f83a0677c
SepiGabi <sepigabi@gmail.com>
2019-12-17 10:37:34 +0100

    Create asyncForEach.js

---
+++ Javascript_TypeScript/asyncForEach.js
@@ -0,0 +1,22 @@
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
+//usage:
+
+const start = async () => {
+  await asyncForEach([1, 2, 3], async (num) => {
+    await waitFor(50);
+    console.log(num);
+  });
+  console.log('Done');
+}
+start();
+
+//Result:
+//1
+//2
+//3
+//Done

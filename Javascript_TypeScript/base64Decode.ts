596d3b83350a68aad0e4a1a3865acda1cb92eb6e
SepiGabi <sepigabi@gmail.com>
2020-01-10 11:50:23 +0100

    Update base64Decode.ts

--- Javascript_TypeScript/base64Decode.ts
+++ Javascript_TypeScript/base64Decode.ts
@@ -1,11 +1,11 @@
 export function b64DecodeUnicode(str: string) {
-if(str){
-    // Going backwards: from bytestream, to percent-encoding, to original string.
-    return decodeURIComponent(atob(str).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-   }).join(''));
+    if(str) {
+        // Going backwards: from bytestream, to percent-encoding, to original string.
+        return decodeURIComponent(atob(str).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
    }
-   else{
-    return null;
+   else {
+        return null;
    }
 }

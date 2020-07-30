4361a6f1bb181b0bed08631dfbf581eb3c5dfc8f
SepiGabi <sepigabi@gmail.com>
2020-07-30 09:03:03 +0200

    Create CursorExample.sql

---
+++ SQL/CursorExample.sql
@@ -0,0 +1,41 @@
+       
+       declare 
+             @ugyfelId int,
+             @kerdoivId int,
+             @statusCode nvarchar(1),
+             @modifierUserId bigint,
+             @SWP_Ret_Value int
+
+       select @modifierUserId = 0
+
+       create table #InsertedFoos (CID int, KID int, StatusCode nvarchar(2) )
+
+       insert into #InsertedFoos
+       select 
+             l.CID,
+             l.KID,
+             ll.NewClosedStateCode
+       from Foo l
+       join #FoomenFoos ll
+             on ll.FooId = l.ID
+       where ll.NewClosedStateCode is not null
+
+       declare fooCursor cursor forward_only
+       for
+       select 
+             CID,
+             KID,
+             StatusCode
+       from #InsertedFoos
+       open fooCursor
+
+       fetch next from fooCursor into @ugyfelId, @kerdoivId, @statusCode
+
+       while @@fetch_status = 0
+       begin
+             exec sp_set_foo_status @al_ugyfel_id = @ugyfelId, @al_kerdoiv_id = @kerdoivId, @as_status_code = @statusCode, @al_ModifierUserID = @modifierUserId, @SWP_Ret_Value = @SWP_Ret_Value output
+             fetch next from leadCursor into @ugyfelId, @kerdoivId, @statusCode
+       end
+
+       close fooCursor
+       deallocate fooCursor

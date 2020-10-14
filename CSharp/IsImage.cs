97a1114c6e0cabe4629b641a18e48be356fc7f6f
SepiGabi <sepigabi@gmail.com>
2020-10-14 11:28:52 +0200

    Update IsImage.cs

--- CSharp/IsImage.cs
+++ CSharp/IsImage.cs
@@ -1,4 +1,53 @@
- private bool IsImage( Stream stream )
+ 
+ private static List<List<byte>> acceptableImgFileHeaders;
+
+        static DocumentHandler()
+        {
+            List<byte> jpg = new List<byte> { 0xFF, 0xD8 };
+            List<byte> bmp = new List<byte> { 0x42, 0x4D };
+            List<byte> gif = new List<byte> { 0x47, 0x49, 0x46 };
+            List<byte> png = new List<byte> { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A };
+            acceptableImgFileHeaders = new List<List<byte>> { jpg, bmp, gif, png };
+        }
+
+
+private bool IsImage( Stream stream )
+        {
+            stream.Seek( 0, SeekOrigin.Begin );
+            List<byte> bytesIterated = new List<byte>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                int bit = stream.ReadByte();
+                if (bit < 0)
+                {
+                    break;
+                }
+                
+                bytesIterated.Add( (byte)bit );
+
+                bool isImage = acceptableImgFileHeaders.Any( img => !img.Except( bytesIterated ).Any() );
+                if (isImage)
+                {
+                    stream.Seek( 0, SeekOrigin.Begin );
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+Lehet így is: 
+//------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+private bool IsImage( Stream stream )
         {
             stream.Seek( 0, SeekOrigin.Begin );
 
